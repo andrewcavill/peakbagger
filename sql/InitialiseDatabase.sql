76034883cec42bdd9d2b521342a0308eee7892e6
Andrew Cavill <andrew.cavill.nz@gmail.com>
2019-11-16 21:12:10 +1300

    Start setting up database schema and inserting static data

---
+++ sql/InitialiseDatabase.sql
@@ -0,0 +1,43 @@
+-- Drop tables
+DROP TABLE "Peaks";
+DROP TABLE "Regions";
+DROP TABLE "Grades";
+
+-- Create table for regions
+CREATE TABLE "Regions" (
+"Id" INTEGER PRIMARY KEY, 
+"Name" CHARACTER VARYING NOT NULL
+);
+
+-- Create table for grades
+CREATE TABLE "Grades" (
+"Id" INTEGER PRIMARY KEY, 
+"Name" CHARACTER VARYING NOT NULL
+);
+
+-- Create tables for peaks
+CREATE TABLE "Peaks" (
+"Id" SERIAL PRIMARY KEY, 
+"Name" CHARACTER VARYING NOT NULL, 
+"Location" CHARACTER VARYING NOT NULL,
+"RegionId" INTEGER NOT NULL REFERENCES "Regions"("Id"),
+"GradeId" INTEGER NOT NULL REFERENCES "Grades"("Id")
+);
+
+-- Create indexes for peaks
+CREATE INDEX "Peaks_RegionId" ON "Peaks"("RegionId");
+CREATE INDEX "Peaks_GradeId" ON "Peaks"("GradeId");
+
+-- Insert regions
+INSERT INTO "Regions" VALUES (1, 'Northland');
+INSERT INTO "Regions" VALUES (2, 'Waikato');
+INSERT INTO "Regions" VALUES (3, 'Auckland');
+
+-- Insert grades
+INSERT INTO "Grades" VALUES (1, 'Easy');
+INSERT INTO "Grades" VALUES (2, 'Medium');
+INSERT INTO "Grades" VALUES (3, 'Hard');
+
+-- Insert peaks
+INSERT INTO "Peaks" ("Name", "Location", "RegionId", "GradeId") VALUES ('Pirongia', 'Pirongia Forest Park', 2, 2);
+
